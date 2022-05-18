30875b9ececa0b931a653af74952b3944f920244
minseong-dev <aa971020@gmail.com>
2022-05-18 18:50:14 +0900

    이미지에서 RGB채널 분리

---
+++ opencv_channel.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+img_color = cv2.imread('test.jpg', cv2.IMREAD_COLOR )
+
+img_b,img_g,img_r = cv2.split(img_color)
+
+zeros = np.zeros((img_color.shape[0], img_color.shape[1]), dtype="uint8")
+img_b = cv2.merge([img_b, zeros, zeros])
+img_g = cv2.merge([zeros, img_g, zeros])
+img_r = cv2.merge([zeros, zeros, img_r])
+
+cv2.imshow("BGR", img_color)
+cv2.imshow("B", img_b)
+cv2.imshow("G", img_g)
+cv2.imshow("R", img_r)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
